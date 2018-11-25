bbcf490f4ecbeef3ae611f9dac3fb1dbaf1ccf2e
BMeu <bastian@bastianmeyer.eu>
2018-11-25 20:58:42 +0100

    Fix wrong endpoint

--- app/application.py
+++ app/application.py
@@ -129,7 +129,7 @@ def _initialize_extensions(application: Flask) -> None:
     login.init_app(application)
     login.login_message = _l('Please log in to access this page.')
     login.login_message_category = 'error'
-    login.login_view = 'authorization.login'
+    login.login_view = 'userprofile.login'
 
     mail.init_app(application)
 
