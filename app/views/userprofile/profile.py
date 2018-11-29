1a1ba403e665861360df8c78383484e65565708d
BMeu <bastian@bastianmeyer.eu>
2018-11-29 06:52:47 +0100

    Rename form variables

--- app/views/userprofile/profile.py
+++ app/views/userprofile/profile.py
@@ -34,37 +34,37 @@ def user_profile() -> str:
         :return: The HTML response.
     """
 
-    delete_form = DeleteUserProfileForm()
-
-    form = UserProfileForm(obj=current_user, email=current_user.get_email())
-    if form.validate_on_submit():
+    profile_form = UserProfileForm(obj=current_user, email=current_user.get_email())
+    if profile_form.validate_on_submit():
 
         # Always change the name.
         user = User.load_from_id(current_user.get_id())
-        user.name = form.name.data
+        user.name = profile_form.name.data
 
         # If the user entered a password, change that as well.
-        if form.password.data:
-            user.set_password(form.password.data)
+        if profile_form.password.data:
+            user.set_password(profile_form.password.data)
 
         # Write the changes to the database.
         db.session.commit()
 
         # If the email address changed send a confirmation mail to the new address.
-        if user.get_email() != form.email.data:
-            token = user.send_change_email_address_email(form.email.data)
+        if user.get_email() != profile_form.email.data:
+            token = user.send_change_email_address_email(profile_form.email.data)
 
             validity = token.get_validity(in_minutes=True)
             flash(Markup(_('An email has been sent to the new address %(email)s. Please open the link included in the \
                             mail within the next %(validity)d minutes to confirm your new email address. Otherwise, \
                             your email address will not be changed.',
-                           email='<em>{email}</em>'.format(email=form.email.data), validity=validity)),
+                           email='<em>{email}</em>'.format(email=profile_form.email.data), validity=validity)),
                   category='warning')
 
         flash(_('Your changes have been saved.'))
         return redirect(url_for('userprofile.user_profile'))
 
-    return render_template('userprofile/profile.html', title=_('User Profile'), form=form, delete_form=delete_form)
+    delete_form = DeleteUserProfileForm()
+    return render_template('userprofile/profile.html', title=_('User Profile'), profile_form=profile_form,
+                           delete_form=delete_form)
 
 
 @bp.route('/change-email-address/<string:token>')
